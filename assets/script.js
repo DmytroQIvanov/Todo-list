581989c29ecfa655ecf8ae2a70a67755b1c72530
DmytroQIvanov <20IvanovDmitriy03@gmail.com>
2021-01-26 00:00:06 +0200

    v 0.5  add task (task name, task description), delete task

---
+++ assets/script.js
@@ -0,0 +1,50 @@
+let inputTaskName = document.getElementById("input-task-name");
+let inputTaskDescription = document.getElementById("input-task-description");
+
+// let dataArray = JSON.parse(localStorage.getItem("localData"));
+let dataArray = [];
+
+document.addEventListener("keypress", (event) => {
+  if (event.key == "Enter") {
+    addTask();
+  }
+});
+
+function addTask(
+  taskName = inputTaskName.value,
+  taskDescription = inputTaskDescription.value
+) {
+  let out = "";
+  if (taskName != "") {
+    dataArray.push({
+      taskNameArray: taskName,
+      taskDescriptionArray: taskDescription,
+    });
+  }
+  dataArray.map(function (name) {
+    out += `
+      <div class="task-container">
+      <div class="task-name">${name.taskNameArray}</div> 
+      <div class="task-description">${name.taskDescriptionArray}</div>
+      <button onclick="taskDelete(this)">Delete</button>
+      </div>`;
+  });
+  alert(dataArray);
+  document.querySelector("#el").innerHTML = " ";
+
+  document.querySelector("#el").innerHTML = out;
+
+  let data = new Date();
+
+  // alert(data.getDate());
+
+  inputTaskName.value = "";
+  inputTaskDescription.value = "";
+  taskBool = false;
+}
+
+function taskDelete(elem) {
+  dataArray.splice(0, 1);
+  alert(dataArray);
+  addTask();
+}
